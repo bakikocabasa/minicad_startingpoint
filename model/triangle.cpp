6bcc447e81e9a516307483fc6d16b69ae044f619
Hari6315 <h.raju@tu-braunschweig.de>
2022-08-15 21:13:58 +0200

    latest
    no major changes

--- model/triangle.cpp
+++ model/triangle.cpp
@@ -12,21 +12,7 @@ Shape(x,y, observer), length(len)
 
 
 void Triangle::draw(ui::Canvas* canvas) const
-/*{
-    const int half_length = int(length/2);
 
-    for (int y=0;  y<length; y++) {
-        canvas->drawPixel(center.x - half_length, center.y - half_length + y);
-        canvas->drawPixel(center.x + half_length, center.y - half_length + y);
-    }
-
-    for (int x=0;  x<length + (length+1)%2; x++) {
-        canvas->drawPixel(center.x - half_length + x, center.y - half_length);
-        canvas->drawPixel(center.x - half_length + x, center.y + half_length);
-    }
-    canvas->printAtPos(center.x, center.y, std::to_string(id));
-    canvas->refresh();
-}*/
     {
         const int drag_coeff=int((length/2/(2*sqrt(3.0))));
 
