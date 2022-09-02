d0b49da239f5258dd1090a462121009fc6ec27e1
bakikocabasa <bakikocabasa@gmail.com>
2022-09-02 09:37:10 +0200

    Update triangle.cpp

--- model/triangle.cpp
+++ model/triangle.cpp
@@ -21,11 +21,12 @@ void Triangle::draw(ui::Canvas* canvas) const
         const int drag_coeff=int((length/2/(2*sqrt(3.0))));
 
 
-        canvas->drawLine(center.x , center.y -(drag_coeff+1),center.x+half_length ,center.y-(drag_coeff+1));// horizontal line to the right
-        canvas->drawLine(center.x+half_length,center.y-(drag_coeff+1) ,center.x,center.y +half_length-(drag_coeff+1)); // angled line on the right corner
-        canvas->drawLine(center.x-half_length ,center.y-(drag_coeff+1) ,center.x,center.y +half_length-(drag_coeff+1)); // angled line on the left corner
-        canvas->drawLine(center.x,center.y-(drag_coeff+1),center.x-half_length,center.y-(drag_coeff+1)); // horizontal line to the left
-
+        canvas->drawLine(center.x- half_length, center.y -(drag_coeff+1),center.x+half_length ,center.y-(drag_coeff+1));// horizontal line from left to right (1)
+        canvas->drawLine(center.x+half_length,center.y-(drag_coeff+1) ,center.x,center.y +half_length-(drag_coeff+1)); // angled line on the right corner     (3)
+        //first coordinate is the right bottom corner
+        canvas->drawLine(center.x-half_length ,center.y-(drag_coeff+1) ,center.x,center.y +half_length-(drag_coeff+1)); // angled line on the left corner     (2)
+        //first coordinate is the left bottom corner
+      
 
         canvas->printAtPos(center.x, center.y, std::to_string(id));
         canvas->refresh();
